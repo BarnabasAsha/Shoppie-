f5b7b72aac49198c563b03c440cae0d23a62a549
Barnabas Asha <barnabee58@gmail.com>
2020-10-03 20:29:27 +0100

    Fixess

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -28,6 +28,9 @@ class List extends Component {
 
     componentDidUpdate() {
         localStorage.setItem('storeNominations', JSON.stringify(this.state.nominations))
+        this.state.nominations.length > 0 ? (
+            this.setState({toggleList: !this.state.toggleList})
+        ) : null
     }
 
     updateNomations(movieID) {
