0bed624c21724d28eb9facc8cb81294cfd366d73
Barnabas Asha <barnabee58@gmail.com>
2021-01-21 03:40:51 +0000

    hero redesign

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Link } from '@reach/router'
-import '../styles/header.css'
+import React from "react";
+import { Link } from "@reach/router";
+import "../styles/header.css";
 
-function Header () {
-    return (
-        <header className="header">
-            <Link to="/" className="banner" role="banner">
-                Shoppies
-            </Link>
-            <section className="hero_text">
-                Nominate your favorite movies for the Shoppies Movies Award.
-                <br />One of your nominated movies stands the chance of winning the Award!
-            </section>
-        </header>
-    )
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/" className="banner" role="banner">
+        Shoppies
+      </Link>
+
+      <section className="hero">
+        <section className="hero_text">
+          Nominate your favorite movies for the Shoppies Movies Award.
+          <br />
+          One of your nominated movies stands the chance of winning the Award!
+        </section>
+        <div className="hero_grid">
+          <div className="box box-1"></div>
+          <div className="box box-2"></div>
+          <div className="box box-3"></div>
+          <div className="box box-4"></div>
+        </div>
+      </section>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
